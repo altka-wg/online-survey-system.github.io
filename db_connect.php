00a03290dbc29926435f6f064f020913cbbab7b6
aagii <a49201602@gmail.com>
2023-05-03 16:07:35 +0800

    db conf

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
-<? $conn= new mysqli('localhost','root','','survey_db')or die("Could not connect to mysql".mysqli_error($con));
+<? $conn= new mysqli('204.11.58.166','survey_db','3Rh4&ot16','survey_db')or die("Could not connect to mysql".mysqli_error($con));
 mysqli_query($conn,"SET CHARACTER SET 'utf8'");
 mysqli_query($conn,"SET SESSION collation_connection ='utf8_unicode_ci'");
